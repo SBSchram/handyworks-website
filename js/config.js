25c66a88a3d2fbb70807d6387461ab7f872f4af2
Steven Schram <sbschram@gmail.com>
2025-12-13 11:52:25 -0500

    invoice creation

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 // Configuration file for DRY principle
 // Single source of truth for cache busting and other settings
 window.HandyWorksConfig = {
-    version: '20251212v3',
+    version: '20251213v1',
     cacheBust: true,
     
     // Header configuration
