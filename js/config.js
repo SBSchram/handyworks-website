00e6cd38220965f12fb8b443ed7c24f79639418a
Steven Schram <sbschram@gmail.com>
2025-12-13 20:54:34 -0500

    cache

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 // Configuration file for DRY principle
 // Single source of truth for cache busting and other settings
 window.HandyWorksConfig = {
-    version: '20251213v1',
+    version: '20251213v2',
     cacheBust: true,
     
     // Header configuration
