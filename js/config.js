90f8e30459f6c8be97f4e3be8fdeab3d0137d276
Steven Schram <sbschram@gmail.com>
2025-12-14 23:34:46 -0500

    better receipts and acks after payments

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 // Configuration file for DRY principle
 // Single source of truth for cache busting and other settings
 window.HandyWorksConfig = {
-    version: '20251214v20',
+    version: '20251214v22',
     cacheBust: true,
     
     // Header configuration
