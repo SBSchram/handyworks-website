a515978c48d73c92374f6cc6a870bd595bd6dde5
Steven Schram <sbschram@gmail.com>
2025-12-18 11:57:53 -0500

    Change date display to YYYY-MM-DD format with color coding

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 // Configuration file for DRY principle
 // Single source of truth for cache busting and other settings
 window.HandyWorksConfig = {
-    version: '20251217v7',
+    version: '20251217v9',
     cacheBust: true,
     
     // Header configuration
