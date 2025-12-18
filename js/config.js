7b04730254e39b5877790e747876bc65f7ec6c51
Steven Schram <sbschram@gmail.com>
2025-12-18 11:44:41 -0500

    Update cache busting to v6

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 // Configuration file for DRY principle
 // Single source of truth for cache busting and other settings
 window.HandyWorksConfig = {
-    version: '20251217v5',
+    version: '20251217v6',
     cacheBust: true,
     
     // Header configuration
