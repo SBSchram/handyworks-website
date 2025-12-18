d86211f5bde7c94b08a7b40e07ce6c352ea43703
Steven Schram <sbschram@gmail.com>
2025-12-18 15:49:27 -0500

    Right-align dashes to match number columns

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 // Configuration file for DRY principle
 // Single source of truth for cache busting and other settings
 window.HandyWorksConfig = {
-    version: '20251217v14',
+    version: '20251217v15',
     cacheBust: true,
     
     // Header configuration
