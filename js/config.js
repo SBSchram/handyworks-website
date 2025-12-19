09db6855bf74e7bf52d2a45359cbccdd620c2286
Steven Schram <sbschram@gmail.com>
2025-12-19 07:07:02 -0500

    tighter dashboard

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 // Configuration file for DRY principle
 // Single source of truth for cache busting and other settings
 window.HandyWorksConfig = {
-    version: '20251218v3',
+    version: '20251218v4',
     cacheBust: true,
     
     // Header configuration
