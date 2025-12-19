a2c793f1dde816895003984d248dfa5258cf8a09
Steven Schram <sbschram@gmail.com>
2025-12-19 08:21:52 -0500

    firebase

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 // Configuration file for DRY principle
 // Single source of truth for cache busting and other settings
 window.HandyWorksConfig = {
-    version: '20251218v12',
+    version: '20251218v14',
     cacheBust: true,
     
     // Header configuration
